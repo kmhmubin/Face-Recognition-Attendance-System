328193800757d8a36d72e9165f73c3580e775f82
Adeetya Upadhyay <49282677+AdeetyaU@users.noreply.github.com>
2020-08-30 18:29:19 +0530

    Update Recognize.py
    Initially, the program would often mistakenly identify me as someone else, and therefore, would save their name instead of mine. The program now displays the confidence factor below the person's face and only if conf > 68 will it make the attendance, else it would not register it. Removed the Unknown images, as it was heavy on the PC and had no actual requirement. Added few more flags and factors, so that face recognition is stricter and made the name displayed on top remove the [] brackets and commas, so it displays only the name and ID.

--- FRAS/Recognize.py
+++ FRAS/Recognize.py
@@ -18,31 +18,57 @@ def recognize_attendence():
     col_names = ['Id', 'Name', 'Date', 'Time']
     attendance = pd.DataFrame(columns=col_names)
 
+    # Initialize and start realtime video capture
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640)  # set video width
+    cam.set(4, 480)  # set video height
+    # Define min window size to be recognized as a face
+    minW = 0.1 * cam.get(3)
+    minH = 0.1 * cam.get(4)
+
     while True:
         ret, im = cam.read()
         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray, 1.2, 5)
+        faces = faceCascade.detectMultiScale(gray, 1.2, 5,minSize = (int(minW), int(minH)),flags = cv2.CASCADE_SCALE_IMAGE)
         for(x, y, w, h) in faces:
-            cv2.rectangle(im, (x, y), (x+w, y+h), (225, 0, 0), 2)
+            cv2.rectangle(im, (x, y), (x+w, y+h), (10, 159, 255), 2)
             Id, conf = recognizer.predict(gray[y:y+h, x:x+w])
 
-            if(conf < 50):
-                ts = time.time()
-                date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
-                timeStamp = datetime.datetime.fromtimestamp(
-                    ts).strftime('%H:%M:%S')
+            if conf < 100:
+
                 aa = df.loc[df['Id'] == Id]['Name'].values
+                confstr = "  {0}%".format(round(100 - conf))
                 tt = str(Id)+"-"+aa
-                attendance.loc[len(attendance)] = [Id, aa, date, timeStamp]
+
 
             else:
-                Id = 'Unknown'
+                Id = '  Unknown  '
                 tt = str(Id)
-            if(conf > 75):
-                noOfFile = len(os.listdir("ImagesUnknown"))+1
-                cv2.imwrite("ImagesUnknown"+os.sep+"Image"+str(noOfFile) +
-                            ".jpg", im[y:y+h, x:x+w])
-            cv2.putText(im, str(tt), (x, y+h), font, 1, (255, 255, 255), 2)
+                confstr = "  {0}%".format(round(100 - conf))
+
+            if (100-conf) > 68:
+                ts = time.time()
+                date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+                timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+                aa = str(aa)[2:-2]
+                attendance.loc[len(attendance)] = [Id, aa, date, timeStamp]
+
+            tt = str(tt)[2:-2]
+            if(100-conf) > 68:
+                tt = tt + " [Pass]"
+                cv2.putText(im, str(tt), (x+5,y-5), font, 1, (255, 255, 255), 2)
+            else:
+                cv2.putText(im, str(tt), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+
+            if (100-conf) > 67:
+                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font,1, (0, 255, 0),1 )
+            elif (100-conf) > 50:
+                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font, 1, (0, 255, 255), 1)
+            else:
+                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font, 1, (0, 0, 255), 1)
+
+
+
         attendance = attendance.drop_duplicates(subset=['Id'], keep='first')
         cv2.imshow('im', im)
         if (cv2.waitKey(1) == ord('q')):
@@ -56,4 +82,4 @@ def recognize_attendence():
     cam.release()
     cv2.destroyAllWindows()
 
-    print("Attendance Successfull")
+    print("Attendance Successful")
