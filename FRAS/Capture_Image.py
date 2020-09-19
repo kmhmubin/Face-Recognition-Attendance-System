dd4359b51cf22098f48ef4ed3bfebcd24cc3b3b1
peace0563 <akul.maurya@miskaa.in>
2020-09-19 17:48:31 +0530

    Fixed name variable to accept string with space and dot too

--- FRAS/Capture_Image.py
+++ FRAS/Capture_Image.py
@@ -1,5 +1,5 @@
 import csv
-
+import re
 import cv2
 import os
 
@@ -24,16 +24,21 @@ def is_number(s):
     return False
 
 
+def check_name(name):
+    matches = re.findall(r"^[ .A-Za-z]+$", name)
+    if len(matches) > 0:
+        return True
+    return False
+
 
 # Take image function
 
 def takeImages():
-    
 
     Id = input("Enter Your Id: ")
     name = input("Enter Your Name: ")
 
-    if(is_number(Id) and name.isalpha()):
+    if(is_number(Id) and check_name(name)):
         cam = cv2.VideoCapture(0)
         harcascadePath = "haarcascade_frontalface_default.xml"
         detector = cv2.CascadeClassifier(harcascadePath)
@@ -43,16 +48,16 @@ def takeImages():
             ret, img = cam.read()
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             faces = detector.detectMultiScale(gray, 1.3, 5)
-            for(x,y,w,h) in faces:
+            for(x, y, w, h) in faces:
                 cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-                #incrementing sample number
+                # incrementing sample number
                 sampleNum = sampleNum+1
-                #saving the captured face in the dataset folder TrainingImage
-                cv2.imwrite("TrainingImage" + os.sep +name + "."+Id + '.' +
+                # saving the captured face in the dataset folder TrainingImage
+                cv2.imwrite("TrainingImage" + os.sep + name + "."+Id + '.' +
                             str(sampleNum) + ".jpg", gray[y:y+h, x:x+w])
-                #display the frame
+                # display the frame
                 cv2.imshow('frame', img)
-            #wait for 100 miliseconds
+            # wait for 100 miliseconds
             if cv2.waitKey(100) & 0xFF == ord('q'):
                 break
             # break if the sample number is morethan 100
@@ -69,7 +74,8 @@ def takeImages():
     else:
         if(is_number(Id)):
             print("Enter Alphabetical Name")
+        elif(not check_name(name)):
+            print("Name can only contain Alphabets, Space or [.]")
+
         if(name.isalpha()):
             print("Enter Numeric ID")
-
-
